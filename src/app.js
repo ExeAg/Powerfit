a6532a48c17b9d10616d3bd0342af205e415e593
ExeAg <112787222+ExeAg@users.noreply.github.com>
2023-09-06 15:43:13 -0300

    Miercoles 2

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
+app.use((0, morgan_1.default)('dev'));
 exports.default = app;

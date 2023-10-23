3942cef33fdf56b6c889eb0f623ddaa373f48d81
ExeAg <112787222+ExeAg@users.noreply.github.com>
2023-10-23 14:20:09 -0300

    Chat2

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js"
 import cors from 'cors'
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+import { resolve, dirname } from "path";
+import { PORT } from "./configio.js";
 
 const app = express();
 
@@ -15,9 +19,27 @@ app.use(cors({
 app.use(cookieParser());
 app.use(morgan("dev")); //dev es para que muestre un mensaje corto por console
 app.use(express.json()); //convierte los req.body en formato JSON
+app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(resolve("/api")));
 //Rutas
 app.use("/api", authRoutes); //para que todas las authRoutes empiecen con /api
 app.use("/api", taskRoutes);
+
+const server = http.createServer(app);
+const io = new SocketServer(server, {});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (body) => {
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(8),
+    });
+  });
+});
+
+server.listen(PORT);
+console.log(`Server on port ${PORT}`);
 
 export default app;

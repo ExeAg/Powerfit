25ca8df6f4d242db9821e8fabff4d9194e3d3406
ExeAg <112787222+ExeAg@users.noreply.github.com>
2023-10-28 11:43:12 -0300

    Home

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ function HomePage() {
   return (
     <div className="bg-image bg-cover" style={{ backgroundImage: `url(/images/image.jpg)` }}>
     <h1>HomePage</h1>
-    <p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p><p>sdasd</p>
+    <p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p><p>.</p>
     </div>
   )
 }

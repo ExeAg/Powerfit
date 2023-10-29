247a747d21a45eacd192544c134f97f4a5d9e69c
ExeAg <112787222+ExeAg@users.noreply.github.com>
2023-10-29 03:54:56 -0300

    CCS addComp

--- client/src/pages/CompFormPage.jsx
+++ client/src/pages/CompFormPage.jsx
@@ -11,66 +11,77 @@ function CompFormPage() {
 
   return (
     <div className="flex h-[calc(100vh-100px)] items-center justify-center">
-      <form onSubmit={onSubmit}>
-        <input
-          type="number"
-          placeholder="Altura"
-          {...register("altura")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          autoFocus
-        />
-        <input
-          type="number"
-          placeholder="peso"
-          {...register("peso")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="grasa"
-          {...register("grasa")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="agua"
-          {...register("agua")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="viceral"
-          {...register("viceral")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="musculo"
-          {...register("musculo")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="proteinas"
-          {...register("proteinas")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="basal"
-          {...register("basal")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <input
-          type="number"
-          placeholder="hueso"
-          {...register("hueso")}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        />
-        <button>
-          Guardar
-        </button>
-      </form>
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <form onSubmit={onSubmit}>
+          <label htmlFor="title">Altura:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("altura")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            autoFocus
+          />
+          <label htmlFor="title">Peso:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("peso")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Grasa Corporal:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("grasa")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Nivel de Agua:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("agua")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Grasa Viceral:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("viceral")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Masa Muscular:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("musculo")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Proteínas:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("proteinas")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Metabolismo Basal:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("basal")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <label htmlFor="title">Masa ósea:</label>
+          <input
+            type="number"
+            placeholder="Valor"
+            {...register("hueso")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <button className="bg-indigo-500 px-3 py-2 rounded-md my-2">
+            Guardar
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
